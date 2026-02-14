5259d9ac84724a34b1561eb8833df1db40dcb9e6
Adnane <rchidiadnan1@gmail.com>
2026-02-14 15:31:55 +0100

    Add Salutation component to see how props work

--- lab1/src/App.jsx
+++ lab1/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import MyComponent from './components/MyComponent'
+import Salutation from './components/Salutation'
 
 function App() {
   return(
     <>
       <h1>Hello React!</h1>
-      <MyComponent message={'Click so the number will increase by one at a time.'} />
+      <Salutation name={'Adnane'} age={19}/>
+      <MyComponent message={'Click so the number will increase by one at a time'} />
     </>
   ) 
 }
---
+++ lab1/src/components/Salutation.jsx
@@ -0,0 +1,10 @@
+function Salutation({name, age}) {
+    return(
+        <>
+            <p>My name is {name}</p>
+            <p>My age is {age}</p>
+        </>
+    )
+}
+
+export default Salutation;
